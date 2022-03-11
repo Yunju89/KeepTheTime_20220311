1cff9bc3044e32cdfb9b21bb25442d595c7691c4
Yunju89 <toyaann@naver.com>
2022-03-11 16:42:07 +0900

    인터페이스에, 로그인 기능을 위한 함수 명세 작성

--- app/src/main/java/org/techtown/keepthetime_20220311/api/APIList.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/api/APIList.kt
@@ -1,4 +1,18 @@
 package org.techtown.keepthetime_20220311.api
 
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.POST
+
 interface APIList {
+
+//    BASE_URL 에 해당하는 서버에서, 어떤 기능들을 사용할건지 명시.
+
+    @POST("/user")
+    fun postRequestLogin(             // @ Field 안에 키값을 담아줌
+        @Field("email") email : String ,
+        @Field("password") pw : String )  : Call<JSONObject>
+
+
 }
