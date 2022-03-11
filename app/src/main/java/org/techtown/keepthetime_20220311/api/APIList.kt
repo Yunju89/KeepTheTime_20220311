82d8541515c18298eb5f8cf4a1e2e855879c186d
Yunju89 <toyaann@naver.com>
2022-03-11 16:42:58 +0900

    인터페이스에, 로그인 기능을 위한 함수 명세 작성

--- app/src/main/java/org/techtown/keepthetime_20220311/api/APIList.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/api/APIList.kt
@@ -3,12 +3,14 @@ package org.techtown.keepthetime_20220311.api
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface APIList {
 
 //    BASE_URL 에 해당하는 서버에서, 어떤 기능들을 사용할건지 명시.
 
+    @FormUrlEncoded     // 파라미터 중에, Field (formData)에 담아야 하는 파라미터가 있다면.
     @POST("/user")
     fun postRequestLogin(             // @ Field 안에 키값을 담아줌
         @Field("email") email : String ,
