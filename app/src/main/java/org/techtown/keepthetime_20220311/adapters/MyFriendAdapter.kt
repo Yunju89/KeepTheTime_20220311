8b7dbda9ef7270d9f913d56977c5bcb41c605358
Yunju89 <toyaann@naver.com>
2022-03-14 15:22:24 +0900

    친구의 가입 경로에 따른 이메일 주소 표시 - 소셜 로그인 분기 처리

--- app/src/main/java/org/techtown/keepthetime_20220311/adapters/MyFriendAdapter.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/adapters/MyFriendAdapter.kt
@@ -30,15 +30,40 @@ class MyFriendAdapter(
         val txtEmail = row.findViewById<TextView>(R.id.txtEmail)
         val txtNickname = row.findViewById<TextView>(R.id.txtNickname)
         val imgProfile = row.findViewById<ImageView>(R.id.imgProfile)
+        val imgSocialLoginLogo = row.findViewById<ImageView>(R.id.imgSocialLoginLogo)
 
         Glide.with(mContext).load(data.profile_img).into(imgProfile)
         txtEmail.text = data.email
         txtNickname.text = data.nick_name
 
+//        사용자의 provider - "default" : 이메일 표시, "kakao" : 카카오 로그인, "facebook" : 페북, "naver" : 네이버 로그인
+
+        when (data.provider){
+            "default" -> {
+//                이메일 표시
+                txtEmail.text = data.email
+//                로고 이미지 숨김
+            }
+            "kakao" -> {
+//                카카오 로그인
+                txtEmail.text = "카카오 로그인"
+//                로고 이미지 : 카카오 아아디
+            }
+            "facebook" -> {
+                txtEmail.text = "페북 로그인"
+            }
+            "naver" -> {
+                txtEmail.text = "네이버 로그인"
+            }
+            else -> {
+
+            }
+
+        }
 
 
 
 
-        return row
+       return row
     }
 }
