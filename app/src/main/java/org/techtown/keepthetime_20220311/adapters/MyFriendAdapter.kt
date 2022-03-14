20e80db5825b3780db2142d3006a4889a8ec5de3
Yunju89 <toyaann@naver.com>
2022-03-14 15:13:34 +0900

    친구 목록 실제 데이터 반영

--- app/src/main/java/org/techtown/keepthetime_20220311/adapters/MyFriendAdapter.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/adapters/MyFriendAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import org.techtown.keepthetime_20220311.R
 import org.techtown.keepthetime_20220311.datas.UserData
 
@@ -21,6 +24,17 @@ class MyFriendAdapter(
             tmpRow = LayoutInflater.from(mContext).inflate(R.layout.my_friend_list_item, null)
         }
         val row = tmpRow!!
+
+        val data = mList[position]
+
+        val txtEmail = row.findViewById<TextView>(R.id.txtEmail)
+        val txtNickname = row.findViewById<TextView>(R.id.txtNickname)
+        val imgProfile = row.findViewById<ImageView>(R.id.imgProfile)
+
+        Glide.with(mContext).load(data.profile_img).into(imgProfile)
+        txtEmail.text = data.email
+        txtNickname.text = data.nick_name
+
 
 
 
