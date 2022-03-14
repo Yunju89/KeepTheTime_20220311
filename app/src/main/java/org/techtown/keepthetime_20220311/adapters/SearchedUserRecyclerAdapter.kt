ff0fdffdaf2cb03d7655d590ff092cfc8ee7d26c
Yunju89 <toyaann@naver.com>
2022-03-14 16:12:22 +0900

    리싸이클러뷰의 어댑터 제작 중  - onCreateViewHolder 함수 내용 작성  - ~list_item inflate 해와서  - MyViewHolder 객체 생성의 재료로 활용.

--- app/src/main/java/org/techtown/keepthetime_20220311/adapters/SearchedUserRecyclerAdapter.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/adapters/SearchedUserRecyclerAdapter.kt
@@ -2,9 +2,11 @@ package org.techtown.keepthetime_20220311.adapters
 
 import android.content.Context
 import android.service.autofill.UserData
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import org.techtown.keepthetime_20220311.R
 
 class SearchedUserRecyclerAdapter(
     val mContext: Context,
@@ -16,6 +18,12 @@ class SearchedUserRecyclerAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+//        xml inflate 해와서 => 이를 가지고, MyViewHolder 객체로 생성. 리턴
+//        재사용성을 알아서 보존해줌.
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.searched_user_list_item, parent, false)
+        return MyViewHolder( row )
 
     }
 
