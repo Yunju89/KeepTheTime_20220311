1094487fcb6b9f936ed4bcf98199fc5c51d47d39
Yunju89 <toyaann@naver.com>
2022-03-14 14:44:43 +0900

    리스트뷰 어댑터 뼈대작업 마무리

--- app/src/main/java/org/techtown/keepthetime_20220311/adapters/MyFriendAdapter.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/adapters/MyFriendAdapter.kt
@@ -1,7 +1,11 @@
 package org.techtown.keepthetime_20220311.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import org.techtown.keepthetime_20220311.R
 import org.techtown.keepthetime_20220311.datas.UserData
 
 class MyFriendAdapter(
@@ -10,4 +14,17 @@ class MyFriendAdapter(
     val mList : List<UserData>
 ) : ArrayAdapter<UserData>(mContext, resId, mList) {
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tmpRow = convertView
+        if(tmpRow == null){
+            tmpRow = LayoutInflater.from(mContext).inflate(R.layout.my_friend_list_item, null)
+        }
+        val row = tmpRow!!
+
+
+
+
+        return row
+    }
 }
