ff3e47c39f1ed5f282a4769bec9fc6e9d7c6140c
Yunju89 <toyaann@naver.com>
2022-03-14 12:40:17 +0900

    친구 목록 화면 진입

--- app/src/main/java/org/techtown/keepthetime_20220311/fragments/MyProfileFragment.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/fragments/MyProfileFragment.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import org.techtown.keepthetime_20220311.ManageMyFriendsActivity
 import org.techtown.keepthetime_20220311.R
 import org.techtown.keepthetime_20220311.SplashActivity
 import org.techtown.keepthetime_20220311.databinding.FragmentMyProfileBinding
@@ -41,6 +42,13 @@ class MyProfileFragment : BaseFragment(){
     }
 
     override fun setupEvents() {
+
+        binding.btnManageMyFriends.setOnClickListener {
+
+            val myIntent = Intent(mContext,ManageMyFriendsActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
         binding.btnLogout.setOnClickListener {
 
