a9f0c2e1c53c7db6dd717d4bfbf77d21a2c09da1
Yunju89 <toyaann@naver.com>
2022-03-14 12:38:52 +0900

    내 친구 목록 관리 화면 추가

---
+++ app/src/main/java/org/techtown/keepthetime_20220311/ManageMyFriendsActivity.kt
@@ -0,0 +1,28 @@
+package org.techtown.keepthetime_20220311
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.techtown.keepthetime_20220311.databinding.ActivityManageMyFriendsBinding
+
+class ManageMyFriendsActivity : BaseActivity() {
+
+    lateinit var binding : ActivityManageMyFriendsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_manage_my_friends)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+
+    }
+}
