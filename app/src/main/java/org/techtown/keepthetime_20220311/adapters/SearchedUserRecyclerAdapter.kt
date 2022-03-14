a51da69785efc739b1cb7dc2ef966093c54a6c68
Yunju89 <toyaann@naver.com>
2022-03-14 17:17:50 +0900

    실제 데이터 반영 완료  - getView 함수에 적는 요령과 같다.

--- app/src/main/java/org/techtown/keepthetime_20220311/adapters/SearchedUserRecyclerAdapter.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/adapters/SearchedUserRecyclerAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import org.techtown.keepthetime_20220311.R
 import org.techtown.keepthetime_20220311.datas.UserData
 
@@ -25,10 +26,33 @@ class SearchedUserRecyclerAdapter(
         val imgSocialLoginLogo = view.findViewById<ImageView>(R.id.imgSocialLoginLogo)
         val btnAddFriend = view.findViewById<Button>(R.id.btnAddFriend)
 
-//        실 데이터 반영 기능이 있는 함수
-        fun bind( data : UserData ) {
+        //        실 데이터 반영 기능이 있는 함수
+        fun bind(data: UserData) {
             txtNickname.text = data.nick_name
-            txtNickname.text = data.email
+            Glide.with(mContext).load(data.profile_img).into(imgProfile)
+
+            when(data.provider) {
+                "default" -> {
+                    imgSocialLoginLogo.visibility = View.GONE
+                    txtEmail.text = data.email
+                }
+                "kakao" -> {
+                    imgSocialLoginLogo.visibility = View.VISIBLE
+                    imgSocialLoginLogo.setImageResource(R.drawable.kakao_logo)
+                    txtEmail.text = "카카오 로그인"
+                }
+                "facebook" -> {
+                    imgSocialLoginLogo.visibility = View.VISIBLE
+                    imgSocialLoginLogo.setImageResource(R.drawable.facebook_logo)
+                    txtEmail.text = "페이스북 로그인"
+                }
+                "naver" -> {
+                    imgSocialLoginLogo.visibility = View.VISIBLE
+                    imgSocialLoginLogo.setImageResource(R.drawable.naver_logo)
+                    txtEmail.text = "네이버 로그인"
+                }
+            }
+
         }
 
     }
