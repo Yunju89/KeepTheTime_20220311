017035338e8decc41ab30fa1a178268fbc5fb15a
Yunju89 <toyaann@naver.com>
2022-03-14 14:56:51 +0900

    내 친구목록을 ArrayList에 담아주기 시도 중  - 레트로피승로 내 친구 목록 불러내는 API 호출

--- app/src/main/java/org/techtown/keepthetime_20220311/ManageMyFriendsActivity.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/ManageMyFriendsActivity.kt
@@ -3,10 +3,18 @@ package org.techtown.keepthetime_20220311
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import org.techtown.keepthetime_20220311.databinding.ActivityManageMyFriendsBinding
+import org.techtown.keepthetime_20220311.datas.BasicResponse
+import org.techtown.keepthetime_20220311.datas.UserData
+import org.techtown.keepthetime_20220311.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ManageMyFriendsActivity : BaseActivity() {
 
     lateinit var binding : ActivityManageMyFriendsBinding
+
+    val mFriendList = ArrayList<UserData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +31,27 @@ class ManageMyFriendsActivity : BaseActivity() {
 
     override fun setValues() {
 
+        getMyFriendListFromServer()
 
     }
+
+    fun getMyFriendListFromServer(){
+
+        apiList.getRequestMyFriendList(
+            ContextUtil.getLoginUserToken(mContext),
+            "my"    // 수락 완료된 친구 목록만 불러오기
+        ).enqueue(object : Callback<BasicResponse>{
+
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+            }
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+
+
+    }
+
 }
