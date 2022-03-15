43d01a0d3e86109a0e8ddcd7a85f81ef8ea4567c
Yunju89 <toyaann@naver.com>
2022-03-15 15:31:04 +0900

    날짜 선택하는 DatePickerDialog 예제  - 월에 대한 추가 조치 필요

--- app/src/main/java/org/techtown/keepthetime_20220311/EditAppointmentActivity.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/EditAppointmentActivity.kt
@@ -1,7 +1,9 @@
 package org.techtown.keepthetime_20220311
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import org.techtown.keepthetime_20220311.databinding.ActivityEditAppointmentBinding
 
@@ -18,6 +20,23 @@ class EditAppointmentActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.txtDate.setOnClickListener {
+            val dsl = object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                    Toast.makeText(mContext, "${year}년 ${month}월 ${dayOfMonth}일 선택함", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            val dpd = DatePickerDialog(
+                mContext,
+                dsl,
+                2022,
+                3,
+                15
+            ).show()
+
+        }
 
     }
 
