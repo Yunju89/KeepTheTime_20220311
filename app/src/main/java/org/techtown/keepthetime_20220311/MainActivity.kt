42e52118ed472a7d522b32a7ab94c5d7e0cf529f
Yunju89 <toyaann@naver.com>
2022-03-15 14:49:43 +0900

    뷰 페이저의 페이지 이동 > 바텀 네비게이션의 선택 메뉴 선정  - 뷰페이저2 / 바텀 네비게이션 연동

--- app/src/main/java/org/techtown/keepthetime_20220311/MainActivity.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/MainActivity.kt
@@ -3,6 +3,7 @@ package org.techtown.keepthetime_20220311
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
 import org.techtown.keepthetime_20220311.adapters.MainViewPager2Adapter
 import org.techtown.keepthetime_20220311.adapters.MainViewPagerAdapter
 import org.techtown.keepthetime_20220311.databinding.ActivityMainBinding
@@ -14,12 +15,12 @@ import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setupEvents()
         setValues()
@@ -33,20 +34,36 @@ class MainActivity : BaseActivity() {
 
 //            어떤 메뉴가 선택되었는지? it 변수가 알려줌.
 
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.myAppointment -> binding.mainViewPager2.currentItem = 0
-                R.id.myProfile -> binding.mainViewPager2.currentItem=1
+                R.id.myProfile -> binding.mainViewPager2.currentItem = 1
             }
 
             return@setOnItemSelectedListener true
         }
 
 //        뷰페이저의 페이지 이동 > 바텀 네비게이션의 메뉴 선택
+        binding.mainViewPager2.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+//            추상메쏘드 X, 이벤트처리 함수를 직접 오버라이딩
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                binding.mainBottomNav.selectedItemId = when(position){
+                    0 -> R.id.myAppointment
+                    else -> R.id.myProfile
+                }
+
+            }
+        })
+
 
     }
+
     override fun setValues() {
 
-        binding.mainViewPager2.adapter = MainViewPager2Adapter(this)    // 변수 : Activity => 객체 : Context 대입 불가,
+        binding.mainViewPager2.adapter =
+            MainViewPager2Adapter(this)    // 변수 : Activity => 객체 : Context 대입 불가,
 
     }
 
