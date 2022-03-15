6140a21b6225bfcff7b854be7d51746d072ec163
Yunju89 <toyaann@naver.com>
2022-03-15 14:58:20 +0900

    약속 등록 화면 추가

--- app/src/main/java/org/techtown/keepthetime_20220311/fragments/AppointmentListFragment.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/fragments/AppointmentListFragment.kt
@@ -1,11 +1,13 @@
 package org.techtown.keepthetime_20220311.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import org.techtown.keepthetime_20220311.EditAppointmentActivity
 import org.techtown.keepthetime_20220311.R
 import org.techtown.keepthetime_20220311.databinding.FragmentAppointmentListBinding
 
@@ -33,11 +35,16 @@ class AppointmentListFragment : BaseFragment(){
 
     override fun setupEvents() {
 
+        binding.btnAddAppointment.setOnClickListener {
+            val myIntent = Intent(mContext, EditAppointmentActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 
     override fun setValues() {
 
-        binding.txtTest.text = "코틀린에서 문구 바꾸기"
 
 
 
