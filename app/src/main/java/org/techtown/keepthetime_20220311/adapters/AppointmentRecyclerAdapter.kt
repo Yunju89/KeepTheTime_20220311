2f430ab6742bfb5c1132960a106438828e99f928
Yunju89 <toyaann@naver.com>
2022-03-16 10:41:13 +0900

    약속 목록 어댑터 추가

--- app/src/main/java/org/techtown/keepthetime_20220311/adapters/AppointmentRecyclerAdapter.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/adapters/AppointmentRecyclerAdapter.kt
@@ -1,27 +1,37 @@
 package org.techtown.keepthetime_20220311.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import org.techtown.keepthetime_20220311.R
 import org.techtown.keepthetime_20220311.datas.AppointmentData
 
 class AppointmentRecyclerAdapter(
-    val mContext : Context,
-    val mList : List<AppointmentData>
-) : RecyclerView.Adapter<AppointmentRecyclerAdapter.MyViewHolder>(){
+    val mContext: Context,
+    val mList: List<AppointmentData>
+) : RecyclerView.Adapter<AppointmentRecyclerAdapter.MyViewHolder>() {
 
-    inner class MyViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+        fun bind (data:AppointmentData){
+
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
+        val view = LayoutInflater.from(mContext).inflate(R.layout.appointment_list_item,parent,false)
+        return MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
+        val data = mList[position]
+        holder.bind(data)       // 홀더가 가지고 있는 바인드 함수에 데이터 넣어서 실행
+
     }
 
-    override fun getItemCount
+    override fun getItemCount() = mList.size
 }
