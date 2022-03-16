89785d857ed52994e80e36b4220dbe67d19f82e1
Yunju89 <toyaann@naver.com>
2022-03-16 16:21:42 +0900

    길찾기 응답 확인  - 상세 파싱은 진행중

--- app/src/main/java/org/techtown/keepthetime_20220311/ViewMapActivity.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/ViewMapActivity.kt
@@ -3,6 +3,7 @@ package org.techtown.keepthetime_20220311
 import android.graphics.Camera
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
@@ -10,6 +11,10 @@ import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.PathOverlay
+import com.odsay.odsayandroidsdk.API
+import com.odsay.odsayandroidsdk.ODsayData
+import com.odsay.odsayandroidsdk.ODsayService
+import com.odsay.odsayandroidsdk.OnResultCallbackListener
 import org.techtown.keepthetime_20220311.databinding.ActivityViewMapBinding
 import org.techtown.keepthetime_20220311.datas.AppointmentData
 
@@ -51,6 +56,45 @@ class ViewMapActivity : BaseActivity() {
             val marker = Marker()
             marker.position = destLatLng
             marker.map = naverMap
+
+//            Path  객체의 좌표를 설정, => naverMap 추가
+            val path = PathOverlay()
+
+//            path.coords => 출발지 / 도착지만 넣으면 일직선 연결
+
+//            출발지~도착지 사이의 정거장이 있다면, 정거장들을 좌표로 추가
+
+            val myODsayService = ODsayService.init(mContext, "9Xh+Oz2ktqZJHEOLmRJL5TekT/lucTE3zXGFzwZ5otA")
+
+            myODsayService.requestSearchPubTransPath(
+                mAppointment.start_longitude.toString(),
+                mAppointment.start_latitude.toString(),
+                mAppointment.longitude.toString(),
+                mAppointment.latitude.toString(),
+                null,
+                null,
+                null,
+                object : OnResultCallbackListener{
+                    override fun onSuccess(p0: ODsayData?, p1: API?) {
+
+//                        JSONObject > jsonObj받아서 내부 파싱
+                        val jsonObj = p0!!.json
+
+                        Log.d("대중교통길찾기", jsonObj.toString())
+
+                        val resultObj = jsonObj.getJSONObject("result")
+                        Log.d("result", resultObj.toString())
+
+
+                    }
+
+                    override fun onError(p0: Int, p1: String?, p2: API?) {
+
+                    }
+
+                }
+
+            )
 
 
 
