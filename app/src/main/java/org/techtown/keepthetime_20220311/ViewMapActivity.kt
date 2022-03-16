3987ea78c129192c97f69cce3c46dd13451d3ebc
Yunju89 <toyaann@naver.com>
2022-03-16 17:15:52 +0900

    정보창을 우리가만든 xml로 반영  - 네이버 문서의 View 리턴 참고

--- app/src/main/java/org/techtown/keepthetime_20220311/ViewMapActivity.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/ViewMapActivity.kt
@@ -4,6 +4,9 @@ import android.graphics.Camera
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraUpdate
@@ -146,13 +149,30 @@ class ViewMapActivity : BaseActivity() {
 
                         val infoStr = "이동시간 : ${minutes}분, 비용:${payment}원"
 
+//                        정보창 띄우기 => 원하는 모양으로 customView
                         val infoWindow = InfoWindow()
-                        infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(mContext){
-                            override fun getText(p0: InfoWindow): CharSequence {
-                                return infoStr
-                            }
-                        }
 
+                        infoWindow.adapter = object : InfoWindow.DefaultViewAdapter(mContext){
+                            override fun getContentView(p0: InfoWindow): View {
+
+//                                리턴 자료형 : View 리턴.
+//                                View 객체를 만드는 방법? => LayoutInflater 에게 inflate 시키면 > 그 결과물이 View
+
+                                val view = LayoutInflater.from(mContext).inflate(R.layout.destination_info_window,null)
+
+                                val txtPlaceName = view.findViewById<TextView>(R.id.txtPlaceName)
+                                val txtMoveTime = view.findViewById<TextView>(R.id.txtMoveTime)
+                                val txtPayment = view.findViewById<TextView>(R.id.txtPayment)
+
+
+                                txtPlaceName.text = mAppointment.place
+                                txtMoveTime.text = "${minutes}분 소요"
+                                txtPayment.text = "${payment} 원 필요"
+
+                                return view
+                            }
+
+                        }
                         infoWindow.open(marker)
 
                     }
