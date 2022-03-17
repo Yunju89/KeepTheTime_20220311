b243dfcd63b8c7f01324e16b42f65f8d6ab364d4
Yunju89 <toyaann@naver.com>
2022-03-17 12:33:53 +0900

    실제 출발 장소 첨부 + 스크롤뷰 적용  - 지도와 스크롤뷰의 이벤트 충돌

--- app/src/main/java/org/techtown/keepthetime_20220311/api/APIList.kt
+++ app/src/main/java/org/techtown/keepthetime_20220311/api/APIList.kt
@@ -62,6 +62,9 @@ interface APIList {
     fun postRequestAddAppointment(
         @Field("title") title : String,
         @Field("datetime") datetime : String,
+        @Field("start_place") startPlace : String,
+        @Field("start_latitude") startLat : Double,
+        @Field("start_longitude") startLng : Double,
         @Field("place") place : String,
         @Field("latitude") lat : Double,
         @Field("longitude") lng : Double,
