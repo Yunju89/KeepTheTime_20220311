f626ac6ecefe7e1eb4178c009dd3c555e7c185c9
Yunju89 <toyaann@naver.com>
2022-03-17 11:16:54 +0900

    내 장소 편집화면 추가

---
+++ app/src/main/java/org/techtown/keepthetime_20220311/EditMyPlaceActivity.kt
@@ -0,0 +1,26 @@
+package org.techtown.keepthetime_20220311
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.techtown.keepthetime_20220311.databinding.ActivityEditMyPlaceBinding
+
+class EditMyPlaceActivity : BaseActivity() {
+    lateinit var binding : ActivityEditMyPlaceBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_my_place)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
